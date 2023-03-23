879c70e8d9ade75fb6cc39704889a05f053c94b8
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2023-03-23 23:24:38 +0530

    feat: created widgets page with interactivity

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,59 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  markdown: {
+    syntaxHighlight: false,
+    // Applied to .md and .mdx files
+    remarkPlugins: [
+      function () {
+        return function (tree) {
+          // add a class to every code block
+          tree.children.forEach((node) => {
+            if (node.type === "code") {
+              const lang = node.lang || "";
+              if (lang !== "dart") return;
+              const value = node.value || "";
+
+              node.value = `{$ begin main.dart $}
+${value}
+{$ end main.dart $}
+{$ begin test.dart $}
+void main() {
+    final widget = MyWidget();
+    final wrapWithScaffold = widget is! Scaffold;
+    final isMaterialApp = widget is MaterialApp;
+    
+    try {
+        runApp(
+            isMaterialApp ? widget :            
+                MaterialApp(
+                    home: wrapWithScaffold ? Scaffold(
+                        body: Center(
+                            child: widget,
+                        )
+                    ) : widget,
+                ),
+        );
+    } catch (e) {
+        print(e);
+    }
+}
+{$ end test.dart $}
+`;
+
+              node.data = node.data || {};
+              node.data.hProperties = node.data.hProperties || {};
+              node.data.hProperties.className =
+                node.data.hProperties.className || [];
+              node.data.hProperties.className.push(
+                "language-run-dartpad:theme-light:mode-flutter:null_safety-true"
+              );
+            }
+          });
+        };
+      },
+    ],
+  },
   experimental: {
     assets: true,
   },
---
+++ src/widgets/layout/container.md
@@ -0,0 +1,20 @@
+---
+title: Container
+---
+
+```dart
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.red,
+      child: Text('Hello World'),
+    );
+  }
+}
+
+```
